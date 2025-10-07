b6b0260cd0e43273965ba454a00534dbb36880ed
shpatiK <shpatikajtazi@gmail.com>
2025-10-07 12:57:50 +0200

    Vendosja e struktures kryesore te aplikacionit dhe React Router

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { AuthProvider } from "./context/AuthContext"
+import { CartProvider } from "./context/CartContext"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Books from "./pages/Books"
+import BookDetail from "./pages/BookDetail"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Cart from "./pages/Cart"
+import Orders from "./pages/Orders"
+
+function App() {
+  return (
+    <AuthProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <div className="min-h-screen bg-gray-50">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/books" element={<Books />} />
+              <Route path="/books/:id" element={<BookDetail />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/orders" element={<Orders />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </CartProvider>
+    </AuthProvider>
+  )
+}
+
+export default App
